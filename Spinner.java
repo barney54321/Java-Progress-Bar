101d1bb78eabaa97fe53be51747f8ed370ff9d66
Andrew Esteban <andrewesteban1999@gmail.com>
2021-06-30 09:55:35 +1000

    added javadoc to Spinner

--- Spinner.java
+++ Spinner.java
@@ -1,44 +1,71 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Represents a simple terminal Spinner that prints at the given speed until 
+ * stop() is called.
+ */
 public class Spinner {
 
-    private static String[] symbols = {"-", "\\", "|", "/"};
+    /**
+     * The characters used in the spin animation.
+     */
+    private final String[] symbols = {"-", "\\", "|", "/"};
 
+    /**
+     * The boolean used to track whether to continue running.
+     */
     private AtomicBoolean condition;
 
-    private int speed;
+    /**
+     * The time between symbol updates (in milliseconds).
+     */
+    private int waitTime;
 
+    /**
+     * The current index for the spin animation.
+     */
     private int index;
 
-    public Spinner(int speed) {
+    /**
+     * Creates a new Spinner object that uses the provided time between frames.
+     * 
+     * @param waitTime The time between frames in the spin cycle.
+     */
+    public Spinner(int waitTime) {
         this.condition = new AtomicBoolean(true);
-        this.speed = speed;
+        this.waitTime = waitTime;
         this.index = 0;
     }
 
+    /**
+     * Creates a new Spinner object with 100milliseconds between frames.
+     */
     public Spinner() {
         this(100);
     }
 
+    /**
+     * Begins the animation in a new thread.
+     */
     public void run() {
         Thread thread = new Thread(() -> {
             while (this.condition.get()) {
                 try {
                     System.out.print(" " + symbols[index] + "\r");
-                    index++;
-                    index = index % symbols.length;
-                    Thread.sleep(this.speed);
+                    index = (index + 1) % symbols.length;
+                    Thread.sleep(this.waitTime);
                 } catch (InterruptedException e) {
                     ;
                 }
             }
-
-            System.out.println("");
         });
 
         thread.start();
     }
 
+    /**
+     * Ends the animation thread.
+     */
     public void stop() {
         this.condition.set(false);
     }
